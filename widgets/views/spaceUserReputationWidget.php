d3879488f337ad19c253f17ed03929b88835e770
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-03 15:40:27 +0900

    Update spaceUserReputationWidget.php

--- widgets/views/spaceUserReputationWidget.php
+++ widgets/views/spaceUserReputationWidget.php
@@ -10,8 +10,8 @@ use humhub\modules\reputation\models\ReputationUser;
 ?>
 
 <div class="panel panel-default members" id="space-members-panel">
-    <?php echo \humhub\widgets\PanelMenu::widget(['id' => 'space-members-panel']); ?>
-    <div class="panel-heading"><?php echo Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', '<strong>Space User</strong> Reputation'); ?></div>
+    <?= \humhub\widgets\PanelMenu::widget(['id' => 'space-members-panel']); ?>
+    <div class="panel-heading"><?= Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', '<strong>Space User</strong> Reputation'); ?></div>
     <div>
         <?php if (count($users) > 0) : ?>         
             <ul class="media-list">
@@ -35,22 +35,22 @@ use humhub\modules\reputation\models\ReputationUser;
                                     ?>
                                 </strong>
                             </div>
-                            <a href="<?php echo $user->getUrl(); ?>" class="pull-left">
-                                <img class="media-object img-rounded user-image user-<?php echo $user->guid; ?>" alt="32x32"
+                            <a href="<?= $user->getUrl(); ?>" class="pull-left">
+                                <img class="media-object img-rounded user-image user-<?= $user->guid; ?>" alt="32x32"
                                      data-src="holder.js/32x32" style="width: 32px; height: 32px;"
-                                     src="<?php echo $user->getProfileImage()->getUrl(); ?>"
+                                     src="<?= $user->getProfileImage()->getUrl(); ?>"
                                      width="40" height="40"/>
                             </a>
                             <div class="media-body">
                                 <h4 class="media-heading">
-                                    <strong><?php echo Html::a($user->getDisplayName(), $user->getUrl()); ?></strong>                                 
+                                    <strong><?= Html::a($user->getDisplayName(), $user->getUrl()); ?></strong>                                 
                                 </h4>
-                                <h5><?php echo Html::encode($user->profile->title); ?></h5>
+                                <h5><?= Html::encode($user->profile->title); ?></h5>
                                 <?php $tag_count = 0; ?>
                                 <?php if ($user->hasTags()) : ?>
                                     <?php foreach ($user->getTags() as $tag): ?>
                                         <?php if ($tag_count <= 5) { ?>
-                                            <?php echo Html::a(Html::encode($tag), Url::to(['/directory/directory/members', 'keyword' => $tag]), array('class' => 'label label-default')); ?>
+                                            <?= Html::a(Html::encode($tag), Url::to(['/directory/directory/members', 'keyword' => $tag]), array('class' => 'label label-default')); ?>
                                             <?php
                                             $tag_count++;
                                         }
@@ -66,21 +66,21 @@ use humhub\modules\reputation\models\ReputationUser;
         <?php else: ?>
             <div class="media">          
                 <div class="media-body">         
-                    <h4 class="media-heading"><?php echo Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'No Reputation found!'); ?>
+                    <h4 class="media-heading"><?= Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'No Reputation found!'); ?>
                         <i class="fa fa-info-circle tt text-break" data-toggle="tooltip" data-placement="top" 
-                           title="<?php echo Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'You can only see reputation the user shares.'); ?>"></i>
+                           title="<?= Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'You can only see reputation the user shares.'); ?>"></i>
                     </h4>        
-                    <div><?php echo Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'Change your settings to share your Reputation on this Space'); ?></div>
+                    <div><?= Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'Change your settings to share your Reputation on this Space'); ?></div>
                 </div>
             </div>
         <?php endif; ?>
         <div class="panel-body">
             <?php if (count($users) == $maxUsers) : ?>
                 <br>
-                <a href="<?php echo $contentContainer->createUrl('//reputation/space/stats'); ?>" class="btn btn-default btn-sm"><?php echo Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'Show all'); ?></a>
+                <a href="<?= $contentContainer->createUrl('//reputation/space/stats'); ?>" class="btn btn-default btn-sm"><?= Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'Show all'); ?></a>
             <?php endif; ?>
             <div class="pull-right">
-                <?php echo Html::a(Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'Settings'), array('//reputation/profile/config', 'uguid' => Yii::$app->user->guid), array('class' => 'btn btn-default btn-sm')); ?>        
+                <?= Html::a(Yii::t('ReputationModule.widgets_views_spaceUserReputationWidget', 'Settings'), array('//reputation/profile/config', 'uguid' => Yii::$app->user->guid), array('class' => 'btn btn-default btn-sm')); ?>         
             </div>                                   
         </div>
     </div>
